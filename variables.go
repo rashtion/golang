3eeb575cf559d608ccb87e5a876a31ead42b9979
rashtion <64362298+rashtion@users.noreply.github.com>
2020-12-18 12:34:09 +0300

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+// Değişkenler GO dilinde nasıl tanımlanır?
+
+var degiskenAdi = "deger"
+
+// Veya;
+
+degiskenAdi := "deger"
+
+// Tüm kodlar tarafımca öğrenirken yazılmıştır
+}
