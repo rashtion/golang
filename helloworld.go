8549cac2e6727d5638d79bad7a6082009925dafd
rashtion <64362298+rashtion@users.noreply.github.com>
2020-12-18 12:31:26 +0300

    Create helloworld.go

---
+++ helloworld.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Merhaba Dünya!")
+  // Veya;
+  fmt.Print("Merhaba Dünya!")
+  // Veya;
+  fmt.Printf("Merhaba Dünya!")
+
+// GO ile ekrana birşeyler yazdırırken bu üç operatörden birini kullanabiliriz.
+// Kodlarımızı func main() {} yaqzan yerdeki '{}' içine yazıyoruz, ve ana paketleri dosyaya dahil etmeden çalıştıramıyoruz.
+// Tüm kodlar tarafımca öğrenirken yazılmıştır
+
+}
